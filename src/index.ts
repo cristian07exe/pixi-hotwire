f9e16349c78b506fb9930f9dcc24df0e5dfc509c
cristian07exe <cristianruggeri.cr@hotmail.com>
2023-06-06 13:14:13 -0300

    cambi de fotos

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-import { Application, Sprite } from "pixi.js";
+import { Application, Loader, Sprite } from "pixi.js";
 
 const app = new Application({
   view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
   backgroundColor: 0x6495ed,
-  width: 1080,
+  width: 640,
   height: 480,
 });
+Loader.shared.add({ url: "./p.jpg", name: "foto" });
 
-const clampy: Sprite = Sprite.from("p.jpg");
+Loader.shared.add({ url: "./p.jpg", name: "Clampy" });
+Loader.shared.onComplete.add(() => {
+  const clampy: Sprite = Sprite.from("foto");
 
-clampy.anchor.set(0.5);
+  clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 5;
-clampy.y = app.screen.height / 5;
+  clampy.x = app.screen.width / 5;
+  clampy.y = app.screen.height / 5;
 
-app.stage.addChild(clampy);
+  app.stage.addChild(clampy);
+});
+Loader.shared.load();
+console.log("hola");
